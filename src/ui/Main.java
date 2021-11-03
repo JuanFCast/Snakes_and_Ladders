ca6fb013455874f8fac41ce1e0f45fc21dd3f6a6
Juan Camilo <juancaram21@gmail.com>
2021-11-03 18:00:01 -0500

    Was modified Dice class and Winners class

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,6 +2,14 @@ package ui;
 
 import model.Game;
 
+/**
+ * This class contains attributes, relationships, and methods of a winner.
+ * @version 1
+ * @author Juan Felipe Castillo Gomez, https://github.com/JuanFCast
+ * @author Juan Camilo Ramirez Tabares, https://github.com/JCamiloRamirezTabares
+ * @author Jesus David Rodriguez Burbano, https://github.com/JesusD03
+ */
+
 public class Main {
 
 	private Game snakeAndLadders;
