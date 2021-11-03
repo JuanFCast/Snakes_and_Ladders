7d76d9b52fced2840845663e7d8f1ef6cc49c172
Juan Felipe Castillo Gomez <juanfelipe.castillogomez@gmail.com>
2021-11-03 17:41:19 -0500

    It works in the Winners class

--- src/model/Player.java
+++ src/model/Player.java
@@ -4,13 +4,16 @@ public class Player {
 	
 	private char player;
 	private String numPlayer;
+	private int numbMovements;
+	private int diceMovements;
 	
 	//Players
 	Player nextPlayer;
 	
-	public Player(char name, String nump) {
-		player = name;
-		numPlayer = nump;
+	public Player(char symbol) {
+		player = symbol;
+		numbMovements = 0;
+		diceMovements = 0;
 	}
 	
 	//Getters & Setters
@@ -26,6 +29,22 @@ public class Player {
 		return numPlayer;
 	}
 	
+	public int getNumbMovements() {
+		return numbMovements;
+	}
+
+	public void setNumbMovements(int numbMovements) {
+		this.numbMovements = numbMovements;
+	}
+
+	public int getDiceMovements() {
+		return diceMovements;
+	}
+
+	public void setDiceMovements(int diceMovements) {
+		this.diceMovements = diceMovements;
+	}
+
 	
 	//For next
 	public Player getNext() {
@@ -35,6 +54,8 @@ public class Player {
 	public void setNext(Player p) {
 		nextPlayer = p;
 	}
+	
+	
 	
 	public String toString() {
 		return "" + player;
--- src/model/Winners.java
+++ src/model/Winners.java
@@ -1,5 +1,88 @@
 package model;
 
-public class Winners {
+
+/**
+ * This class contains attributes, relationships, and methods of a winner.
+ * @version 1
+ * @author Juan Felipe Castillo Gomez, https://github.com/JuanFCast
+ */
+public class Winners extends Player{
+	private String namePlayer;
+	private Double score;
+	private int position;
+
+	//BST
+	private Winners parent;
+	private Winners left;
+	private Winners right;
+
+
+	public Winners(int position, String namePlayer, Double score, char symbol) {
+		super(symbol);
+		this.position=position;
+		this.namePlayer=namePlayer;
+		this.score=score;
+
+	}
+
+	public static void calculateScore() {
+
+	}
+	
+	
+	//Getters and Setters
+	public String getNamePlayer() {
+		return namePlayer;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+
+
+	//Getters and Setters of the BST
+	public Winners getParent() {
+		return parent;
+	}
+
+	public void setParent(Winners parent) {
+		this.parent = parent;
+	}
+
+	public Winners getLeft() {
+		return left;
+	}
+
+	public void setLeft(Winners left) {
+		this.left = left;
+	}
+
+	public Winners getRight() {
+		return right;
+	}
+
+	public void setRight(Winners right) {
+		this.right = right;
+	}
+
+
+
+	/**
+	 * This method produces a String with all the information about Winner. <br>
+	 * <b>name</b>: toString.<br>
+	 * <b>post</b>: All the information of a Winner was returned. <br>
+	 * @return out in a <code> String </code> variable all the information about a Winner.
+	 */ 
+	public String toString() {
+		return " | " + position + " | " + namePlayer + " | " + get() + " | "  + score + " | ";
+	}
+
+
+
 
 }
